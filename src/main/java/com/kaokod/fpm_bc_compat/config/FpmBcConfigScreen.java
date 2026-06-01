524a56b13cf14527754855dc0b65bf546ffe5d72
twk0d <twkod@pm.me>
2026-06-01 15:56:32 -0300

    Version 0.0.2 release

--- src/main/java/com/kaokod/fpm_bc_compat/config/FpmBcConfigScreen.java
+++ src/main/java/com/kaokod/fpm_bc_compat/config/FpmBcConfigScreen.java
@@ -19,38 +19,33 @@ public class FpmBcConfigScreen {
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         ConfigCategory rightArmCat = builder.getOrCreateCategory(Component.translatable("fpm_bc_compat.config.category.right_arm"));
-        addArmEntries(rightArmCat, entryBuilder, "right",
+        addArmEntries(rightArmCat, entryBuilder, 
             config.rightArmPitchMultiplier, config.rightArmPitchOffset, 
             config.rightArmPitchCurve, config.rightArmPitchCurveFloor, 
             config.rightArmUpperPitchBias, config.rightArmLowerPitchBias,
-            config.rightArmPositionXOffset, config.rightArmPositionYOffset, config.rightArmPositionZOffset);
+            config.rightArmPositionXOffset, config.rightArmPositionYOffset, config.rightArmPositionZOffset,
+            config.rightArmLookUpZShift);
 
         ConfigCategory leftArmCat = builder.getOrCreateCategory(Component.translatable("fpm_bc_compat.config.category.left_arm"));
-        addArmEntries(leftArmCat, entryBuilder, "left",
+        addArmEntries(leftArmCat, entryBuilder, 
             config.leftArmPitchMultiplier, config.leftArmPitchOffset, 
             config.leftArmPitchCurve, config.leftArmPitchCurveFloor, 
             config.leftArmUpperPitchBias, config.leftArmLowerPitchBias,
-            config.leftArmPositionXOffset, config.leftArmPositionYOffset, config.leftArmPositionZOffset);
+            config.leftArmPositionXOffset, config.leftArmPositionYOffset, config.leftArmPositionZOffset,
+            config.leftArmLookUpZShift);
 
         ConfigCategory general = builder.getOrCreateCategory(Component.translatable("fpm_bc_compat.config.category.general"));
-        
         general.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.body_y_offset"), config.bodyYOffset.get())
-                .setDefaultValue(-0.1)
-                .setSaveConsumer(config.bodyYOffset::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.body_y_offset.tooltip"))
-                .build());
+                .setDefaultValue(-0.1).setSaveConsumer(config.bodyYOffset::set).build());
 
         general.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.transition_speed"), config.biasSmoothingSpeed.get())
-                .setDefaultValue(0.1)
-                .setSaveConsumer(config.biasSmoothingSpeed::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.transition_speed.tooltip"))
-                .build());
+                .setDefaultValue(0.1).setSaveConsumer(config.biasSmoothingSpeed::set).build());
 
         builder.setSavingRunnable(() -> FpmBcCompatMod.MOD_LOGGER.info("FPM x Better Combat Configuration Saved."));
         return builder.build();
     }
 
-    private static void addArmEntries(ConfigCategory category, ConfigEntryBuilder entryBuilder, String side,
+    private static void addArmEntries(ConfigCategory category, ConfigEntryBuilder entryBuilder,
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue multiplier,
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue pitchOffset,
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue pitchCurve,
@@ -59,42 +54,41 @@ public class FpmBcConfigScreen {
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue lowerBias,
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue posX,
                                      net.neoforged.neoforge.common.ModConfigSpec.DoubleValue posY,
-                                     net.neoforged.neoforge.common.ModConfigSpec.DoubleValue posZ) {
+                                     net.neoforged.neoforge.common.ModConfigSpec.DoubleValue posZ,
+                                     net.neoforged.neoforge.common.ModConfigSpec.DoubleValue lookUpZShift) {
         
         category.addEntry(entryBuilder.startTextDescription(Component.translatable("fpm_bc_compat.config.header.rotation").withStyle(ChatFormatting.BLUE, ChatFormatting.UNDERLINE)).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.pitch_multiplier"), multiplier.get())
-                .setDefaultValue(0.8).setSaveConsumer(multiplier::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.pitch_multiplier.tooltip")).build());
+                .setDefaultValue(0.8).setSaveConsumer(multiplier::set).build());
         
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.pitch_offset"), pitchOffset.get())
-                .setDefaultValue(0.05).setSaveConsumer(pitchOffset::set).build());
+                .setDefaultValue(0.15).setSaveConsumer(pitchOffset::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.upper_bias"), upperBias.get())
-                .setDefaultValue(0.1).setSaveConsumer(upperBias::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.upper_bias.tooltip")).build());
+                .setDefaultValue(0.1).setSaveConsumer(upperBias::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.lower_bias"), lowerBias.get())
-                .setDefaultValue(0.1).setSaveConsumer(lowerBias::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.lower_bias.tooltip")).build());
+                .setDefaultValue(0.1).setSaveConsumer(lowerBias::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.curve_correction"), pitchCurve.get())
-                .setDefaultValue(0.1).setSaveConsumer(pitchCurve::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.curve_correction.tooltip")).build());
+                .setDefaultValue(0.0).setSaveConsumer(pitchCurve::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.curve_floor"), curveFloor.get())
-                .setDefaultValue(0.75).setSaveConsumer(curveFloor::set)
-                .setTooltip(Component.translatable("fpm_bc_compat.config.curve_floor.tooltip")).build());
+                .setDefaultValue(0.75).setSaveConsumer(curveFloor::set).build());
 
         category.addEntry(entryBuilder.startTextDescription(Component.translatable("fpm_bc_compat.config.header.position").withStyle(ChatFormatting.BLUE, ChatFormatting.UNDERLINE)).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.pos_x"), posX.get())
-                .setDefaultValue(side.equals("right") ? -3.0 : 3.0).setSaveConsumer(posX::set).build());
+                .setDefaultValue(0.0).setSaveConsumer(posX::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.pos_y"), posY.get())
-                .setDefaultValue(1.0).setSaveConsumer(posY::set).build());
+                .setDefaultValue(0.2).setSaveConsumer(posY::set).build());
 
         category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.pos_z"), posZ.get())
-                .setDefaultValue(0.5).setSaveConsumer(posZ::set).build());
+                .setDefaultValue(2.0).setSaveConsumer(posZ::set).build());
+
+        category.addEntry(entryBuilder.startDoubleField(Component.translatable("fpm_bc_compat.config.lookup_z_shift"), lookUpZShift.get())
+                .setDefaultValue(2.0).setSaveConsumer(lookUpZShift::set).build());
     }
 }
